c1e8f836dc14c62a4a09ba71680375c7a37b0c33
Louis Taylor <louis@kragniz.eu>
2016-03-23 18:33:12 +0000

    Add constructor

--- src/eu/kragniz/sailsd/Sailsd.java
+++ src/eu/kragniz/sailsd/Sailsd.java
@@ -1,4 +1,11 @@
 package eu.kragniz.sailsd;
 
 public class Sailsd {
+    private String hostname;
+    private int port;
+
+    public Sailsd(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
 }
