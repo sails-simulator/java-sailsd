a3e901cbe294d4d2704312d323643ce706b34e3a
Louis Taylor <louis@kragniz.eu>
2016-03-25 20:32:34 +0000

    Add connect() method

--- src/eu/kragniz/sailsd/Sailsd.java
+++ src/eu/kragniz/sailsd/Sailsd.java
@@ -1,11 +1,19 @@
 package eu.kragniz.sailsd;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Sailsd {
     private String hostname;
     private int port;
+    Socket sailsdSocket;
 
     public Sailsd(String hostname, int port) {
         this.hostname = hostname;
         this.port = port;
+    }
+
+    public void connect() throws IOException {
+        this.sailsdSocket = new Socket(this.hostname, this.port);
     }
 }
