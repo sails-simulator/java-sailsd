3fe7143d6c3248c2405f9e025cd00390083e5bc6
Louis Taylor <louis@kragniz.eu>
2016-03-27 23:58:05 +0100

    Add sending bytes to sailsd

--- src/eu/kragniz/sailsd/Sailsd.java
+++ src/eu/kragniz/sailsd/Sailsd.java
@@ -1,12 +1,14 @@
 package eu.kragniz.sailsd;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
+import static java.lang.System.arraycopy;
 
 public class Sailsd {
     private String hostname;
     private int port;
-    Socket sailsdSocket;
 
     public Sailsd(String hostname, int port) {
         this.hostname = hostname;
@@ -15,5 +17,22 @@ public class Sailsd {
 
     public Socket getConnection() throws IOException {
         return new Socket(this.hostname, this.port);
+    }
+
+    private byte[] stringToNullBytes(String input) throws UnsupportedEncodingException {
+        byte[] stringBytes = input.getBytes("ISO-8859-1");
+        byte[] nullBytes = new byte[stringBytes.length+1];
+        arraycopy(stringBytes, 0, nullBytes, 0, stringBytes.length);
+        return nullBytes;
+    }
+
+    public String sendMessageString(String message) throws IOException {
+        Socket connection = getConnection();
+        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+        byte[] messageBytes = stringToNullBytes(message);
+        out.write(messageBytes, 0, messageBytes.length);
+        connection.close();
+
+        return "";
     }
 }
--- src/eu/kragniz/sailsd/test/SailsdTest.java
+++ src/eu/kragniz/sailsd/test/SailsdTest.java
@@ -19,4 +19,10 @@ public class SailsdTest {
         Sailsd sailsd = new Sailsd("localhost", 3333);
         sailsd.getConnection();
     }
+
+    @Test
+    public void testSendMessageString() throws Exception {
+        Sailsd sailsd = new Sailsd("localhost", 3333);
+        sailsd.sendMessageString("this should fail");
+    }
 }
